4b209b319714cb011ca7ff7b47e1ef693db79b1a
Yelyzaveta Poddubnaya <>
2020-09-13 21:52:03 +0300

    1-module/3-task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,10 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let strToLowerCase = str.toLowerCase();
+  if(strToLowerCase.indexOf(('1xBet').toLowerCase()) > -1 || strToLowerCase.indexOf(('XXX').toLowerCase()) > -1){
+    return true;
+  }else{
+    return false;
+  }
 }
