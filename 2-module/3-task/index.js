d4ed8a9330bb825bc972391afe153ef7b7997120
Yelyzaveta Poddubnaya <>
2020-09-19 20:07:26 +0300

    2-module/2-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,22 @@
 let calculator = {
-  // ваш код
+  read(var1, var2){
+    this.var1 = var1;
+    this.var2 = var2;
+  },
+  sum(){
+    if(typeof(this.var1 === 'number') && typeof(this.var2 === 'number')){
+      return this.var1 + this.var2;
+    }else{
+      console.log('Not appropriate parameters');
+    }
+  },
+  mul(){
+    if(typeof(this.var1 === 'number') && typeof(this.var2 === 'number')){
+      return this.var1 * this.var2;
+    }else{
+      console.log('Not appropriate parameters');
+    }
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
